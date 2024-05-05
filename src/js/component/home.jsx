8507cf42208401abb8cc64f70e948e9116ca1287
matischlegel <161637922+matischlegel@users.noreply.github.com>
2024-05-05 20:10:54 +0000

    proyecto terminado¿?

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,14 +5,44 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./navbar";
 import Jumbotron from "./jumbotron";
 import { Cards } from "./Cards";
+
+//create your first component
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+import { Cards } from "./Cards";
 import { Finaltext } from "./Finaltext";
 
 //create your first component
 const Home = () => {
 	return (
 		<React.Fragment>
-			<div>
-				<Cards />
+			<div className="container">
+				<div className="cards1 vh-100">
+					<Cards img={"https://s3.amazonaws.com/cdn.wp.m4ecnet/wp-content/uploads/2018/01/16095118/Depositphotos_3667865_m-2015-compressor.jpg"}
+						title={"Card title"}
+						text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor."}
+						button={"Find Out More!"} />
+
+					<Cards img={"https://s3.amazonaws.com/cdn.wp.m4ecnet/wp-content/uploads/2018/01/16095118/Depositphotos_3667865_m-2015-compressor.jpg"}
+						title={"Card title"}
+						text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor."}
+						button={"Find Out More!"} />
+
+					<Cards img={"https://s3.amazonaws.com/cdn.wp.m4ecnet/wp-content/uploads/2018/01/16095118/Depositphotos_3667865_m-2015-compressor.jpg"}
+						title={"Card title"}
+						text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor."}
+						button={"Find Out More!"} />
+
+					<Cards img={"https://s3.amazonaws.com/cdn.wp.m4ecnet/wp-content/uploads/2018/01/16095118/Depositphotos_3667865_m-2015-compressor.jpg"}
+						title={"Card title"}
+						text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor."}
+						button={"Find Out More!"} />
+				</div>
+				<div className="text1">
+					<Finaltext />
+				</div>
 			</div>
 		</React.Fragment>
 	);
