7367356a0ee3ea674274171a54a50bc673750c57
matischlegel <161637922+matischlegel@users.noreply.github.com>
2024-05-05 19:31:22 +0000

    proyecto termiando?

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-export const Cards = () => {
+export const Cards = (props) => {
     return (
         <React.Fragment>
-            <div className="card" style={{ width: "18rem" }}>
-                <img src="..." className="card-img-top" alt="..." />
+            <div className="card">
+                <img src={props.img} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <div className="forPadding">
+                        <h5 className="card-title">{props.title}</h5>
+                        <p className="card-text">{props.text}</p>
+                    </div>
+                    <div className="boton">
+                        <a href="#" className="btn btn-primary">{props.button}</a>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
---
+++ src/js/component/Finaltext.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Finaltext = () => {
+    return (
+        <React.Fragment>
+            <p id="text1">Copyright @ Your Website 2019</p>
+        </React.Fragment>
+    )
+}
